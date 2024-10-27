a1ce8b07aec02f8e59b8be0f91fb7e738e88cd9f
brandoncahuecc <brandoncahuecc@outlook.com>
2024-10-26 21:46:53 -0600

    Clase 02 - Hola Mundo y Declaración y Asignación de Variables

---
+++ Clase1Consola/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//Estamos declarando e inicializando las variables vistas en clase
+int numeroInt = 18;
+long numeroLong = 20;
+float numeroFloat = 20.2f;
+double numeroDouble = 20.40;
+decimal numeroDecimal = 21.48m;
+char cadenaChar = 'A';
+string cadenaString = "BVAA";
+bool condicional = true;
+DateTime fechaActual = DateTime.Now;
+
+/*
+ * Estamos imprimiendo las variables declaradas en la parte superior
+ * Los tipos de variables fueron los vistos en clase
+ */
+Console.WriteLine("int: " + numeroInt);
+Console.WriteLine("long: " + numeroLong);
+Console.WriteLine("float: " + numeroFloat);
+Console.WriteLine("double: " + numeroDouble);
+Console.WriteLine("decimal: " + numeroDecimal);
+Console.WriteLine("char: " + cadenaChar);
+Console.WriteLine("string: " + cadenaString);
+Console.WriteLine("bool: " + condicional);
+Console.WriteLine("DateTime: " + fechaActual);
