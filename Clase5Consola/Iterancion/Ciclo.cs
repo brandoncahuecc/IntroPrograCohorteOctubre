99f3ca75af797f85f7033b8b16946f763ed18976
brandoncahuecc <brandoncahuecc@outlook.com>
2024-11-16 21:48:46 -0600

    Clase 05 - Tipos de Iteración, manejo de arreglos y listas y metodos

---
+++ Clase5Consola/Iterancion/Ciclo.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase5Consola.Iterancion
+{
+    public class Ciclo
+    {
+
+        public void CicloFor()
+        {
+            for (int i = 0; i < 25; i += 2)
+            {
+                Console.WriteLine(i + 1);
+            }
+        }
+
+        public void CicloFor(int recorridos)
+        {
+            for (int i = 0; i < recorridos; i += 2)
+            {
+                Console.WriteLine(i + 1);
+            }
+        }
+
+        public bool CicloWhile()
+        {
+            int x = 100;
+
+            while (x < 110)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+
+            return true;
+        }
+
+        public bool CicloDoWhile()
+        {
+            int x = 100;
+
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            } while (x < 110);
+
+            return true;
+        }
+
+        public void CicloWhile(bool esTrue = false)
+        {
+            while (esTrue)
+            {
+                Console.WriteLine("While Ejecutado");
+                esTrue = false;
+            }
+        }
+
+        public void CicloDoWhile(bool esTrue = false)
+        {
+            do
+            {
+                Console.WriteLine("Do While Ejecutado");
+                esTrue = false;
+            } while (esTrue);
+        }
+
+        public string[] ObtenerArreglo()
+        {
+            string[] cadenas = new string[6];
+
+            cadenas[0] = "A";
+            cadenas[1] = "E";
+            cadenas[2] = "I";
+            cadenas[3] = "O";
+            cadenas[4] = "U";
+
+            return cadenas;
+        }
+
+        public void ImprimirArregloFor(string[] cadenas)
+        {
+            for (int i = 0; i < cadenas.Length; i++)
+            {
+                Console.WriteLine(cadenas[i] ?? "No Value");
+            }
+        }
+
+        public void ImprimirArregloForeach(string[] cadenas)
+        {
+            foreach (string item in cadenas)
+            {
+                Console.WriteLine(item ?? "No Value");
+            }
+        }
+
+        public List<string> ObtenerLista()
+        {
+            List<string> nombres = new() { "Brandon", "Alexander", "Alejandro", "Estuardo", "Mario" };
+            return nombres;
+        }
+
+        public void ImprimirListaForeach(List<string> nombres)
+        {
+            foreach (string item in nombres)
+            {
+                Console.WriteLine(item ?? "No Value");
+            }
+        }
+
+        public void ImprimirListaForEach(List<string> nombres)
+        {
+            nombres.ForEach(item =>
+            {
+                Console.WriteLine(item);
+                //Console.WriteLine("-----------");
+            });
+        }
+
+        public void TipoParametros(string valor, ref string referencia, out string salida, string opcional = "Default")
+        {
+            valor = "Cambie el valor";
+            referencia = "Cambie la referencia";
+            salida = "Cambie la salida";
+            opcional = "Cambie el opcional";
+
+            Console.WriteLine("********************************");
+            Console.WriteLine("Dentro de TipoParametros");
+            Console.WriteLine("********************************");
+            Console.WriteLine($"Valor = {valor}");
+            Console.WriteLine($"Referencia = {referencia}");
+            Console.WriteLine($"salida = {salida}");
+            Console.WriteLine($"opciona = {opcional}");
+        }
+
+    }
+}
---
+++ Clase5Consola/Program.cs
@@ -0,0 +1,86 @@
+﻿
+using Clase5Consola.Iterancion;
+
+Console.WriteLine("Ciclo For");
+Ciclo ciclo = new();
+ciclo.CicloFor();
+
+Console.WriteLine("________________________________");
+Console.WriteLine("Ingrese hasta que número imprimir");
+string numeroInput = Console.ReadLine();
+
+//int numero = 10;
+int.TryParse(numeroInput, out int numero);
+
+ciclo.CicloFor(numero);
+
+
+Console.WriteLine("________________________________");
+Console.WriteLine("Ciclo While");
+
+bool esTrue = false;
+
+ciclo.CicloWhile();
+ciclo.CicloWhile(esTrue);
+
+
+Console.WriteLine("________________________________");
+Console.WriteLine("Ciclo Do While");
+
+ciclo.CicloDoWhile();
+ciclo.CicloDoWhile(esTrue);
+
+
+//esTrue = false;
+
+
+Console.WriteLine("________________________________");
+Console.WriteLine("Arreglos");
+string[] cadenas = ciclo.ObtenerArreglo();
+
+Console.WriteLine("For");
+ciclo.ImprimirArregloFor(cadenas);
+
+Console.WriteLine("Foreach");
+ciclo.ImprimirArregloForeach(cadenas);
+
+
+Console.WriteLine("________________________________");
+Console.WriteLine("Listas");
+List<string> nombres = ciclo.ObtenerLista();
+
+Console.WriteLine("Foreach");
+ciclo.ImprimirListaForeach(nombres);
+
+nombres.Add("Tomas");
+nombres.Add("David");
+
+Console.WriteLine("ForEach");
+ciclo.ImprimirListaForEach(nombres);
+
+Console.WriteLine("________________________________");
+Console.WriteLine("Tipo de Parámetros");
+
+Console.WriteLine("********************************");
+Console.WriteLine("Antes de Ejecutar TipoParametros");
+Console.WriteLine("********************************");
+string valor = "Hola mundo";
+string referencia = "Como estas";
+//string salida = "Adios";
+//string opcional = "Prueba";
+
+Console.WriteLine($"Valor = {valor}");
+Console.WriteLine($"Referencia = {referencia}");
+//Console.WriteLine($"salida = {salida}");
+//Console.WriteLine($"opciona = {opcional}");
+
+ciclo.TipoParametros(valor, ref referencia, out string salida);
+
+Console.WriteLine("********************************");
+Console.WriteLine("Despues de Ejecutar TipoParametros");
+Console.WriteLine("********************************");
+Console.WriteLine($"Valor = {valor}");
+Console.WriteLine($"Referencia = {referencia}");
+Console.WriteLine($"salida = {salida}");
+//Console.WriteLine($"opciona = {opcional}");
+
